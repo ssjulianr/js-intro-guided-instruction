22b25c06da963005a680f5c48a37b3089754511f
Julian <julian@Administrators-MacBook-Pro.local>
2023-04-22 13:46:14 -0400

    Completed GI problems

---
+++ app.js
@@ -0,0 +1,93 @@
+// // VERY EASY 
+// let x = 10;
+// let y = 2;
+
+// let z = (x - y);
+
+// console.log(z);
+
+// // EASY 
+// let num1 = 6;
+// let num2 = 4;
+// let difference = (num1 - num2);
+// const firstName = ("Alaina");
+// const secondName = ("Yves"); 
+
+
+// console.log("The name", firstName, "is longer than the name", secondName, "by", difference, "characters");
+
+//MEDIUM 
+
+
+const letter = prompt();
+
+// ✅ Check if a letter is uppercase
+if (letter.toUpperCase() === letter) {
+  // 👇️ this runs
+  console.log('WHY ARE YOU YELLING?!');
+} else if (letter.toLowerCase() === letter){
+  console.log('why are you whispering?');
+} else {
+    console.log('Thank you for speaking normally!');
+}
+
+// //HARD 
+
+//     //Add
+//     function add(num3, num4){
+//         return num3 + num4
+//     }
+
+//     console.log(add(100, 50));
+
+//     //Subtract
+//     function subtract(num3, num4){
+//         return num3 - num4
+//     }
+
+//     console.log(subtract(100, 50));
+
+//     //Multiply 
+//     function multiply(num3, num4){
+//         return num3 * num4
+//     }
+
+//     console.log(multiply(100, 50));    
+
+//     //Divide 
+//     function divide(num3, num4){
+//         return num3 / num4
+//     }
+
+//     console.log(divide(100, 50)); 
+
+// //VERY HARD 
+
+
+// //Created variable "firstNum". Variable goes into the prompt, console.log records the result 
+// let num3, operator, num4, answer;
+
+// num3 = parseInt(prompt("Insert a number please:"));
+
+
+// operator = prompt("Choose an Operation (+, -, *, or /):");
+
+
+// num4 = parseInt(prompt("Insert a second number:"));
+
+// //Conditional Statements 
+
+// if(operator === "+"){
+//    answer = add(num3, num4);
+// } else if (operator === "-"){
+//     answer = subtract(num3, num4);
+// } else if (operator === "*"){
+//     answer = multiply(num3, num4);
+// } else if (operator === "/"){
+//     answer = divide(num3/num4);
+// }
+// console.log(answer);
+
+
+
+
